bcf27750ff972b30264ad4174e4f03a5dd8bcfae
Saheme <roj_t@mail.ru>
2022-06-05 21:54:11 +0300

    new commit

---
+++ src/lesson1/Calculator.java
@@ -0,0 +1,30 @@
+package lesson1;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int sum = countSum(9, 12);
+        int minus = countMinus(12, 8);
+        int multiply = countMultiply(5, 9);
+        int division = countDivision(12, 6);
+        System.out.println("Сумма " + sum + "\nВычитание " + minus + "\nУмножение " + multiply + "\nДеление " + division);
+
+    }
+
+    public static int countSum(int a, int b) {
+        return a + b;
+    }
+
+    public static int countMinus(int a, int b) {
+        return a - b;
+    }
+
+    public static int countMultiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int countDivision(int a, int b) {
+        return a / b;
+    }
+
+
+}
